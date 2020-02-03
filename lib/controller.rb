3b49f0a3448d758b00e935dfb81072cfbdd07545
Stovenn <jonathan.sylvestre.pro@gmail.com>
2020-02-03 16:48:13 +0100

    djsadjsad

--- lib/controller.rb
+++ lib/controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < Sinatra::Base
 
   post '/gossips/new/' do  
     Gossip.new("#{params["gossip_author"]}", "#{params["gossip_content"]}").save
+    redirect '/'
   end
   
 end
