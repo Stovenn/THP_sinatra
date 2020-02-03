90b4a4a22d82d85122df426555a71e8c652cf540
Stovenn <jonathan.sylvestre.pro@gmail.com>
2020-02-03 16:03:09 +0100

    commit 008

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,3 +1,5 @@
+require 'gossip'
+
 class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
@@ -8,7 +10,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/gossips/new/' do  
-    Gossip.new(les_entrées_du_gossip).save
+    Gossip.new("super_auteur", "super gossip").save
   end
   
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,10 +1,17 @@
 require 'csv'
 
 class Gossip
-def save
+    attr_accessor :author, :content
+  def initialize(author, content)
+    @author = author
+    @content = content
+  end 
 
-end
-
+  def save
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+      csv << [@author, @content]
+    end
+  end
 
 
 end #fin classe
